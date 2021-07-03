291008e0017b8f1761123ea4d49cde7a43cbbc99
raj <raj.rajeev2244@gmail.com>
2021-07-03 10:35:42 +0530

    First Commit

---
+++ src/test/java/com/raj/in/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
